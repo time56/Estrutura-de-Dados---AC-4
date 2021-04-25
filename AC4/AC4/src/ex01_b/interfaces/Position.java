948bafe860caba0fa3055953f60ada5762f885ee
time56 <78887720+time56@users.noreply.github.com>
2021-04-25 18:52:21 -0300

    Delete AC4/AC4 directory

--- AC4/AC4/src/ex01_b/interfaces/Position.java
+++
@@ -1,6 +0,0 @@
-package ex01_b.interfaces;
-
-public interface Position<E>  {
-    // Retorna o elemento armazenado nesta posição.
-    E element();
-}
--- AC4/AC4/src/ex01_c/exceptions/EmptyPriorityQueueException.java
+++
@@ -1,9 +0,0 @@
-package ex01_c.exceptions;
-
-public class EmptyPriorityQueueException extends RuntimeException {
-	
-	public EmptyPriorityQueueException(String err) {
-		super(err);
-	}
-
-}
